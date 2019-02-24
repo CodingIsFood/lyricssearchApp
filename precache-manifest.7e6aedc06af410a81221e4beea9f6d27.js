eab11fdc0b3d185bdbc02e9ad04fcfae1a576c4f
CodingIsFood <ayoodedere@gmail.com>
2019-02-24 19:35:09 +0100

    Updates

---
+++ precache-manifest.7e6aedc06af410a81221e4beea9f6d27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c50c9c03d2373e2a8e9c",
+    "url": "/lyricssearchApp/static/css/main.9f123ba9.chunk.css"
+  },
+  {
+    "revision": "c50c9c03d2373e2a8e9c",
+    "url": "/lyricssearchApp/static/js/main.c50c9c03.chunk.js"
+  },
+  {
+    "revision": "31600236c3aef89bba78",
+    "url": "/lyricssearchApp/static/js/runtime~main.31600236.js"
+  },
+  {
+    "revision": "0957330208057b8183fb",
+    "url": "/lyricssearchApp/static/js/2.09573302.chunk.js"
+  },
+  {
+    "revision": "4c6f14a91ba1db8be8f3f8695d2b3796",
+    "url": "/lyricssearchApp/index.html"
+  }
+];
